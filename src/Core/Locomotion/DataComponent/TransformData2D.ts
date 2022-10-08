3c7ba982a47d79dd57ebabb855fbfd0a74aaa68e
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-10-08 02:04:55 -0500

    Update: TransformData2D Moved the DataComponent to Locomotion.

---
+++ src/Core/Locomotion/DataComponent/TransformData2D.ts
@@ -0,0 +1,24 @@
+import { Component, ComponentSchema } from "ecsy/Component";
+import { Types } from "ecsy/Types";
+import { Vector2, Vector2Type } from "../../../Mathematics/Vector2";
+
+export class TransformData2D extends Component<TransformData2D> {
+  static schema: ComponentSchema = {
+    position: {
+      type: Vector2Type,
+      default: new Vector2(0, 0),
+    },
+    rotation: {
+      type: Types.Number,
+      default: 0,
+    },
+    scale: {
+      type: Vector2Type,
+      default: new Vector2(1, 1),
+    },
+  };
+
+  position!: Vector2;
+  rotation!: number;
+  scale!: Vector2;
+}
