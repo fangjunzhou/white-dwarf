a07e3486ae7edda9b52315c69420946b7557e6a0
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-10-08 13:24:05 -0500

    Update: White Dwarf Editor Removed unused imports.

--- src/Editor/index.ts
+++ src/Editor/index.ts
@@ -1,8 +1,6 @@
 import { mainInit, mainWorld } from "../Core";
 import { LocomotionComponentRegister } from "../Core/Locomotion/LocomotionComponentRegister";
 import { RenderComponentRegister } from "../Core/Render/RenderComponentRegister";
-import { CameraTag } from "../Core/Render/TagComponent/CameraTag";
-import { MainCameraTag } from "../Core/Render/TagComponent/MainCameraTag";
 
 const main = () => {
   console.log("Editor Started");
