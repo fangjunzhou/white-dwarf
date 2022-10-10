6e536c4e653454922eaf870ada3241944ef33e2e
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-10-10 14:07:31 -0500

    Update: White Dwarf Editor Added inspector handler.

--- src/Editor/EditorUIHandler.ts
+++ src/Editor/EditorUIHandler.ts
@@ -1,3 +1,4 @@
+import { Component, ComponentSchema } from "ecsy/Component";
 import { Entity } from "ecsy/Entity";
 import { editorEventContext, editorUIContext } from "./EditorContext";
 
@@ -47,5 +48,51 @@ export const updateEntityList = (entities: Array<Entity>) => {
 };
 
 export const updateEntityInspector = (entity: Entity) => {
-  console.log(`Entity selected: ${entity.name} (${entity.id})`);
+  if (!editorUIContext.entityInspector) {
+    return;
+  }
+
+  // Get all components of entity.
+  const components = entity.getComponents();
+  const componentIndices = Object.keys(components);
+
+  // Traverse all entityInspectors.
+  for (let i = 0; i < editorUIContext.entityInspector.length; i++) {
+    const entityInspector = editorUIContext.entityInspector[i];
+    // Remove all children.
+    while (entityInspector.firstChild) {
+      entityInspector.removeChild(entityInspector.firstChild);
+    }
+
+    // Add components data.
+    for (let j = 0; j < componentIndices.length; j++) {
+      const componentIndex = componentIndices[j];
+      const component = components[componentIndex];
+      const componentSchema = Object.getPrototypeOf(component).constructor
+        .schema as ComponentSchema;
+      const componentDataContent: { [key: string]: any } = {};
+      Object.keys(component).forEach((key) => {
+        if (Object.keys(componentSchema).includes(key)) {
+          componentDataContent[key] = component[key as keyof typeof component];
+        }
+      });
+
+      // Add component name.
+      const componentDiv = document.createElement("div");
+      const componentTitle = document.createElement("span");
+      componentTitle.innerText = component.constructor.name;
+      componentDiv.appendChild(componentTitle);
+
+      // Add component data.
+      const componentData = document.createElement("pre");
+      componentData.innerText = JSON.stringify(componentDataContent, null, " ");
+      componentDiv.appendChild(componentData);
+
+      // Set css class.
+      componentDiv.className = "componentListItem";
+
+      // Add component to entityInspector.
+      entityInspector.appendChild(componentDiv);
+    }
+  }
 };
