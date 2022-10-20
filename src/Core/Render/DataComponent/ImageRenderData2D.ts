85160eccdbbe6bb6b0049985fd16e484e5d97e9f
Fangjun Zhou <fzhou48@wisc.edu>
2022-10-20 14:40:50 -0500

    Update: ImageRenderData2D Added custom inspector for ImageRenderData2D.

--- src/Core/Render/DataComponent/ImageRenderData2D.ts
+++ src/Core/Render/DataComponent/ImageRenderData2D.ts
@@ -1,6 +1,14 @@
-import { Component, ComponentSchema } from "ecsy/Component";
+import {
+  Component,
+  ComponentSchema,
+  COMPONENT_CHANGE_EVENT,
+} from "ecsy/Component";
 import { Types } from "ecsy/Types";
-import { Vector2, Vector2Type } from "../../../Mathematics/Vector2";
+import {
+  Vector2,
+  Vector2CustomEditor,
+  Vector2Type,
+} from "../../../Mathematics/Vector2";
 import { IComponent } from "../../ComponentRegistry";
 
 @IComponent.register
@@ -23,4 +31,53 @@ export class ImageRenderData2D extends Component<ImageRenderData2D> {
   src: string = "";
   img: CanvasImageSource | null = null;
   imageCenter!: Vector2;
+
+  public useDefaultInspector: boolean = false;
+
+  public onInspector: (componentDiv: HTMLDivElement) => void = (
+    componentDiv: HTMLDivElement
+  ) => {
+    // Create src div.
+    const srcDiv = document.createElement("div");
+    srcDiv.style.display = "flex";
+    srcDiv.style.flexDirection = "row";
+
+    srcDiv.appendChild(document.createTextNode("src: "));
+    const srcInput = document.createElement("input");
+    srcInput.value = this.src;
+    srcInput.style.flex = "1";
+    srcInput.addEventListener("change", (event) => {
+      this.src = srcInput.value;
+      this.eventEmitter.emit(COMPONENT_CHANGE_EVENT);
+    });
+    this.eventEmitter.on(COMPONENT_CHANGE_EVENT, () => {
+      // If the input is focused, don't update it.
+      if (document.activeElement !== srcInput) {
+        srcInput.value = this.src;
+      }
+    });
+    srcDiv.appendChild(srcInput);
+
+    // Add image center div.
+    const imageCenterDiv = document.createElement("div");
+    imageCenterDiv.style.display = "flex";
+    imageCenterDiv.style.flexDirection = "column";
+
+    imageCenterDiv.appendChild(document.createTextNode("imageCenter: "));
+    const [imageCenterVector2Div, onImgCenterChanged] = Vector2CustomEditor(
+      this.imageCenter,
+      (value) => {
+        this.imageCenter = value;
+        this.eventEmitter.emit(COMPONENT_CHANGE_EVENT);
+      }
+    );
+    this.eventEmitter.on(COMPONENT_CHANGE_EVENT, () => {
+      onImgCenterChanged(this.imageCenter);
+    });
+    imageCenterVector2Div.style.minWidth = "0px";
+    imageCenterDiv.appendChild(imageCenterVector2Div);
+
+    componentDiv.appendChild(srcDiv);
+    componentDiv.appendChild(imageCenterDiv);
+  };
 }
