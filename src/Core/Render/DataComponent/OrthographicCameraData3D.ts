b85e0f22cb30a9737c25a249035c3400d556f54e
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-10-31 23:46:46 -0500

    Added: OrthographicCameraData3D

---
+++ src/Core/Render/DataComponent/OrthographicCameraData3D.ts
@@ -0,0 +1,44 @@
+import { Component, ComponentSchema } from "ecsy/Component";
+import { Types } from "ecsy/Types";
+
+export class OrthographicCameraData3D extends Component<OrthographicCameraData3D> {
+  static schema: ComponentSchema = {
+    left: {
+      type: Types.Number,
+      default: -1,
+    },
+    right: {
+      type: Types.Number,
+      default: 1,
+    },
+    top: {
+      type: Types.Number,
+      default: 1,
+    },
+    bottom: {
+      type: Types.Number,
+      default: -1,
+    },
+    near: {
+      type: Types.Number,
+      default: 0.1,
+    },
+    far: {
+      type: Types.Number,
+      default: 1000,
+    },
+  };
+
+  // Camera left plane distance.
+  left!: number;
+  // Camera right plane distance.
+  right!: number;
+  // Camera top plane distance.
+  top!: number;
+  // Camera bottom plane distance.
+  bottom!: number;
+  // Camera near plane distance.
+  near!: number;
+  // Camera far plane distance.
+  far!: number;
+}
