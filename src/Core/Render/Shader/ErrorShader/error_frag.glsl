b515c1efffd6dda37e2c066a063da544bd9540c6
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-12-04 01:27:11 -0600

    Added: Shader Compiler Error Shader

--- src/Core/Render/Shader/ErrorShader/error_frag.glsl
+++ src/Core/Render/Shader/ErrorShader/error_frag.glsl
@@ -6,9 +6,6 @@ uniform mat4 uP;
 uniform mat3 uMVn;
 uniform mat4 uMVP;
 
-// // Uniform light data.
-// uniform vec3 uDirLight;
-
 // Camera space position.
 varying vec3 fPosition;
 // Vertex color.
@@ -19,5 +16,5 @@ varying vec3 fNormal;
 varying vec2 fTexCoord;
 
 void main(){
-    gl_FragColor = fColor;
+    gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
 }
