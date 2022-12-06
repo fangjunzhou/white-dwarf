f6607b53f26e34846b245eca6cb1ce335f515898
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-12-05 23:44:27 -0600

    Update: Mesh Added mesh data buffer for mesh generation.

--- src/Core/Render/Mesh.ts
+++ src/Core/Render/Mesh.ts
@@ -1,4 +1,5 @@
 import { cloneClonable, copyCopyable, createType } from "ecsy-wd";
+import { vec2, vec3, vec4 } from "gl-matrix";
 
 export class Mesh {
   vertexPositions!: Float32Array;
@@ -6,6 +7,50 @@ export class Mesh {
   vertexColors!: Float32Array;
   vertexTexCoords!: Float32Array;
   triangleIndices!: Uint8Array;
+
+  vertexPositionBuffer!: Array<number>;
+  vertexNormalBuffer!: Array<number>;
+  vertexColorBuffer!: Array<number>;
+  vertexTexCoordsBuffer!: Array<number>;
+  triangleIndexBuffer!: Array<number>;
+
+  public compileBufferToArrays() {
+    this.vertexPositions = new Float32Array(this.vertexPositionBuffer);
+    this.vertexNormals = new Float32Array(this.vertexNormalBuffer);
+    this.vertexColors = new Float32Array(this.vertexColorBuffer);
+    this.vertexTexCoords = new Float32Array(this.vertexTexCoordsBuffer);
+    this.triangleIndices = new Uint8Array(this.triangleIndexBuffer);
+
+    this.clearBuffers();
+  }
+
+  public clearBuffers() {
+    this.vertexPositionBuffer = [];
+    this.vertexNormalBuffer = [];
+    this.vertexColorBuffer = [];
+    this.vertexTexCoordsBuffer = [];
+    this.triangleIndexBuffer = [];
+  }
+
+  public addVertexPosition(vertex: vec3) {
+    this.vertexPositionBuffer.push(vertex[0], vertex[1], vertex[2]);
+  }
+
+  public addVertexNormal(normal: vec3) {
+    this.vertexNormalBuffer.push(normal[0], normal[1], normal[2]);
+  }
+
+  public addVertexColor(color: vec4) {
+    this.vertexColorBuffer.push(color[0], color[1], color[2], color[3]);
+  }
+
+  public addVertexTexCoords(texCoords: vec2) {
+    this.vertexTexCoordsBuffer.push(texCoords[0], texCoords[1]);
+  }
+
+  public registerTriangle(t1: number, t2: number, t3: number) {
+    this.triangleIndexBuffer.push(t1, t2, t3);
+  }
 
   copy(m: Mesh): Mesh {
     this.vertexPositions = new Float32Array(m.vertexPositions);
